5d8c89dbbe5f47c5dc0b8f1da1aed2fc8dd0d188
_CodeGreen <mapope8703@gmail.com>
2024-01-31 23:04:16 -0500

    Removed inner borders

--- prototypes/styles.lua
+++ prototypes/styles.lua
@@ -11,3 +11,22 @@ styles["bvs_frame_semitransparent"] = {
         }
     }
 }
+
+styles["bvs_finished_game_frame"] = {
+    type = "frame_style",
+    parent = "invisible_frame",
+    graphical_set = {
+        base = {
+            type = "composition",
+            filename = "__better-victory-screen__/graphics/finished_game_frame.png",
+            corner_size = 8,
+            position = {0, 0}
+        }
+    }
+}
+
+styles["bvs_finished_game_table"] = {
+    type = "table_style",
+    parent = "finished_game_table",
+    border = {}
+}
